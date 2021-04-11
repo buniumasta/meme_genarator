cdeab8d441694863db087730314436fb783966f0
Bartosz Ostrowski <buniumasta@gmail.com>
2021-04-11 23:38:13 +0200

    Staged: changes

--- QuoteEngine/quote_mode.py
+++ QuoteEngine/quote_mode.py
@@ -13,19 +13,18 @@ class QuoteMode:
     def quote(self) -> str:
         """Return quote."""
         return self._quote
-    
+
     @property
     def author(self) -> str:
         """Return author."""
         return self._author
-    
+
     @quote.setter
     def quote(self, quote: str):
         """Set quote."""
         self._quote = quote
-    
+
     @author.setter
     def author(self, author: str):
         """Set author."""
         self._author = author
-    
