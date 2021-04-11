70c73b015bda15d7cc61edc6e9b82749be81f558
Bartosz Ostrowski <buniumasta@gmail.com>
2021-04-11 21:25:24 +0200

    CodeStyle ensure pep8

--- QuoteEngine/IngestorInterface.py
+++ QuoteEngine/IngestorInterface.py
@@ -7,13 +7,14 @@ from typing import List
 
 class IngestorInterface(ABC):
     """Abstract classs used Data Source for Quotes."""
+
     @classmethod
-    def can_ingest(cls, path:str) -> bool:
+    def can_ingest(cls, path: str) -> bool:
         """Checks if file extensions is supported."""
         pass
+
     @classmethod
     @abstractmethod
-    def parse(cls, path:str) -> List[QuoteMode]:
+    def parse(cls, path: str) -> List[QuoteMode]:
         """Parses the file and returns List of QuoteModel Objects"""
         pass
-
--- QuoteEngine/QuoteMode.py
+++ QuoteEngine/QuoteMode.py
@@ -3,9 +3,7 @@
 class QuoteMode:
     """Class stores quote and author."""
 
-    def __init__(self, quote:str, author:str):
+    def __init__(self, quote: str, author: str):
         """Initialize QuoteMode Objects"""
         self._quote=quote
         self._author=author
-    
-    
