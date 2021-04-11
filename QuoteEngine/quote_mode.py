1ee414786146253342434eb506e0169518ac393e
Bartosz Ostrowski <buniumasta@gmail.com>
2021-04-11 23:26:47 +0200

    Pylint - Corrections

--- QuoteEngine/quote_mode.py
+++ QuoteEngine/quote_mode.py
@@ -7,24 +7,20 @@ class QuoteMode:
     def __init__(self, quote: str, author: str):
         """Initialize QuoteMode Objects."""
         self._quote = quote
-        self._author = author
-    
+        self._author = author    
     @property
     def quote(self) -> str:
         """Return quote."""
         return self._quote
-
     @property
     def author(self) -> str:
         """Return author."""
         return self._author
-    
-    @setter
+    @quote.setter
     def quote(self, quote: str):
         """Set quote."""
         self._quote = quote
-    
-    @setter
+    @author.setter
     def author(self, author: str):
         """Set author."""
         self._author = author
