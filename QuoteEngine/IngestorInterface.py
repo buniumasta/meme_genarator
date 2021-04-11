dbb6336b4ac90c4b3c9c0f663a1bb1bdb54a2108
Bartosz Ostrowski <buniumasta@gmail.com>
2021-04-11 21:16:30 +0200

    Enhacement: QuodeMode, IngestInterface added

---
+++ QuoteEngine/IngestorInterface.py
@@ -0,0 +1,19 @@
+"""
+Interface used for flexible import of quotes from different sources.
+"""
+from abc import ABC, abstractmethod
+from .QuoteMode import QuoteMode
+from typing import List
+
+class IngestorInterface(ABC):
+    """Abstract classs used Data Source for Quotes."""
+    @classmethod
+    def can_ingest(cls, path:str) -> bool:
+        """Checks if file extensions is supported."""
+        pass
+    @classmethod
+    @abstractmethod
+    def parse(cls, path:str) -> List[QuoteMode]:
+        """Parses the file and returns List of QuoteModel Objects"""
+        pass
+
---
+++ QuoteEngine/QuoteMode.py
@@ -0,0 +1,11 @@
+"""Class stores quote and author."""
+
+class QuoteMode:
+    """Class stores quote and author."""
+
+    def __init__(self, quote:str, author:str):
+        """Initialize QuoteMode Objects"""
+        self._quote=quote
+        self._author=author
+    
+    
