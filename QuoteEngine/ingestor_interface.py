760b0cd529263a8ee236ea6c28433b3dc51f1557
Bartosz Ostrowski <buniumasta@gmail.com>
2021-04-13 10:45:45 +0200

    Enhacement TDD added test for extension

--- QuoteEngine/ingestor_interface.py
+++ QuoteEngine/ingestor_interface.py
@@ -10,14 +10,19 @@ from .quote_mode import QuoteMode
 
 class IngestorInterface(ABC):
     """Abstract classs used Data Source for Quotes."""
+    _supported_files = []
 
     @classmethod
     def can_ingest(cls, path: str) -> bool:
         """Check if file extensions is supported."""
-        print(f"Will make it {path}")
+        return path.split()[-1] in cls.supported_files
 
     @classmethod
     @abstractmethod
     def parse(cls, path: str) -> List[QuoteMode]:
         """Parse the file and returns List of QuoteModel Objects."""
-        print(f"Will make it {path}")
+
+    @classmethod
+    def supported_files(cls) -> List[str]:
+        """Return supported files List."""
+        return cls._supported_files
