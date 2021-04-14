14b59affd861471ab6b7dfa83af4d2a28f797dad
Bartosz Ostrowski <buniumasta@gmail.com>
2021-04-14 17:27:23 +0200

    Test: test_image_basic2

--- memegenerator/test_image_caption.py
+++ memegenerator/test_image_caption.py
@@ -7,8 +7,14 @@ from .image_caption import ImageCaption
 class TestImageCaption(unittest.TestCase):
     """Tests Image_caption class."""
 
-    def test_image_basic(self):
+    def test_image_basic1(self):
         """Basic operation test."""
         testcase = str(ImageCaption('./_data/photos/dog/xander_1.jpg'))
+        expected = "./_data/photos/dog/xander_1.jpg, JPEG, (500, 500)xRGB"
+        self.assertEqual(expected, testcase)
+
+    def test_image_basic2(self):
+        """Basic operation test."""
+        testcase = str(ImageCaption('./_data/photos/dog/xander_2.jpg'))
         expected = "./_data/photos/dog/xander_1.jpg, JPEG, (500, 500)xRGB"
         self.assertEqual(expected, testcase)
