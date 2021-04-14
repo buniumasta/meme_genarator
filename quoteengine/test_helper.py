3158d5f2d7936b35c87eb22389d94d042963e1a4
Bartosz Ostrowski <buniumasta@gmail.com>
2021-04-14 19:21:38 +0200

    Media: Pictures&Files added

--- quoteengine/test_helper.py
+++ quoteengine/test_helper.py
@@ -5,6 +5,17 @@ from .quote_model import QuoteModel
 def get_csvfile_quotemode():
     """Return list of string quotemode objects as it is from CSVfile."""
     expected = [
-        str(QuoteModel('Chase the mailman', 'Skittle')),
-        str(QuoteModel('When in doubt, go shoe-shopping', 'Mr. Paws'))]
+        str(QuoteModel('Every artist was first an amateur', 'Ralph Waldo Emerson')),
+        str(QuoteModel('Creativity takes courage', 'Henri Matisse')),
+        str(QuoteModel('Every child is an artist. ' \
+                        'The problem is how to remain ' \
+                        'an artist once we grow up', 'Pablo Picasso')),
+        str(QuoteModel('You don’t take a photograph - you make it', 'Ansel Adams')),
+        str(QuoteModel('Art enables us to find ourselves ' \
+                       'and lose ourselves at the same time', 
+                       'Thomas Merton')),
+        str(QuoteModel('We don’t make mistakes - just happy little accidents', 'Bob Ross')),
+        str(QuoteModel('Have no fear of perfection - you’ll never reach it', 'Salvador Dali')),
+        str(QuoteModel('You can’t wait for inspiration - ' \
+                       'you have to go after it with a club', 'Jack London'))]
     return expected
