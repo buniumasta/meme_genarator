19a29bdb696ebdcdc6c4eb2a8f246551d18045e7
Bartosz Ostrowski <buniumasta@gmail.com>
2021-04-14 13:50:34 +0200

    Enhacement: ImageCaption class added

---
+++ memegenerator/image_caption.py
@@ -0,0 +1,14 @@
+"""Module defines image with caption capability."""
+
+
+class ImageCaption():
+    """Image class."""
+
+    def __init__(self, image_path):
+        """Initialize image caption object"""
+
+    def image_resize(self, width: str):
+        """Re-size image to width"""
+
+    def add_caption(self, body: str, author: str):
+        """Add caption to an image with random place"""
