28f4bdbd9783f09159fe853553dc3a3569a5b4a3
Bartosz Ostrowski <buniumasta@gmail.com>
2021-04-14 11:39:04 +0200

    Enhacement: MemeAgneine class added

---
+++ MemeGenerator/meme_engine.py
@@ -0,0 +1,17 @@
+"""Meme generator Engine."""
+
+
+class MemeEngine():
+    """MemeEngine class generates meme on pictures"""
+
+    def __init__(self, output_dir: str):
+        """Initializes MemeEngine Object."""
+        self._output_dir = output_dir
+
+    def make_meme(self, img_path, text, author, width=500) -> str:  # generated image path
+        """Make meme and return generated image path"""
+
+    @property
+    def output_dir(self) -> str:
+        """Return output_dir."""
+        return self._output_dir
