97885f082ac4b7ee1b6c84b20aabb2512ef93854
Bartosz Ostrowski <buniumasta@gmail.com>
2021-04-14 11:47:29 +0200

    DocStyle

--- MemeGenerator/meme_engine.py
+++ MemeGenerator/meme_engine.py
@@ -2,14 +2,14 @@
 
 
 class MemeEngine():
-    """MemeEngine class generates meme on pictures"""
+    """MemeEngine class generates meme on pictures."""
 
     def __init__(self, output_dir: str):
-        """Initializes MemeEngine Object."""
+        """Initialize MemeEngine Object."""
         self._output_dir = output_dir
 
     def make_meme(self, img_path, text, author, width=500) -> str:
-        """Make meme and return generated image path"""
+        """Make meme and return generated image path."""
 
     @property
     def output_dir(self) -> str:
