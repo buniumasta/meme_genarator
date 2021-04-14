96116eff3c3b6ffe947b1e7dfaa41dc0069876d9
Bartosz Ostrowski <buniumasta@gmail.com>
2021-04-14 18:11:36 +0200

    Tests:ImageCaption tests added

--- memegenerator/image_caption.py
+++ memegenerator/image_caption.py
@@ -4,25 +4,32 @@ from PIL import Image
 
 
 class ImageCaption():
-    """Image class."""
+    """Image Caption classs"""
 
     def __init__(self, image_path):
         """Initialize image caption object."""
         self.image_path = image_path
         try:
             with Image.open(image_path) as self.image:
-                path = os.getcwd()
+                pass
         except OSError:
             self.image = None
-            print(f'Cannot open:{image_path}, cwd: {path}')
 
-    def image_resize(self, width: str):
+    def image_resize(self, width: int):
         """Re-size image to width."""
+
+    def resize_needed(self, width: int) -> bool:
+        """Check if resize is needed"""
+        if self.image.size[1] != width:
+            return True
+        return False
 
     def add_caption(self, body: str, author: str):
         """Add caption to an image with random place."""
 
     def __str__(self):
         """Provide information about the picture."""
-        return f'{self.image_path}, {self.image.format},' \
-            f' {self.image.size}x{self.image.mode}'
+        if self.image is not None:
+            return f'{self.image_path}, {self.image.format},' \
+                f' {self.image.size}x{self.image.mode}'
+        return ""
