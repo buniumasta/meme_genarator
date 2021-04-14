5c25236ac583cd316750df764f79d907b2e4263b
Bartosz Ostrowski <buniumasta@gmail.com>
2021-04-14 13:52:54 +0200

    CodeStyle: image_caption corrected

--- memegenerator/image_caption.py
+++ memegenerator/image_caption.py
@@ -5,10 +5,11 @@ class ImageCaption():
     """Image class."""
 
     def __init__(self, image_path):
-        """Initialize image caption object"""
+        """Initialize image caption object."""
 
     def image_resize(self, width: str):
-        """Re-size image to width"""
+        """Re-size image to width."""
 
     def add_caption(self, body: str, author: str):
-        """Add caption to an image with random place"""
+        """Add caption to an image with random place."""
+
