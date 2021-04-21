ec04da21cd1a11c0e6847843484fe6a289b2cd90
Bartosz Ostrowski <buniumasta@gmail.com>
2021-04-21 21:04:08 +0200

    update

--- quoteengine/ingestor_txt.py
+++ quoteengine/ingestor_txt.py
@@ -14,11 +14,18 @@ class TXTIngestor(IngestorInterface):
         """Parse the TXT file and return List of QuoteMode."""
         if not cls.can_ingest(path):
             return []
-
         quotemode_list = []
-        with open(path, encoding='utf-8-sig') as file:
-            for line in file.readlines():
-                row = line.strip().split('-')
-                quote_author = QuoteModel(row[0].strip(), row[1].strip())
-                quotemode_list.append(quote_author)
-        return quotemode_list
+        try:
+            with open(path, encoding='utf-8-sig') as file:
+                for line in file.readlines():
+                    row = line.strip().split('-')
+                    quote_author = QuoteModel(row[0].strip(), row[1].strip())
+                    quotemode_list.append(quote_author)
+            return quotemode_list
+
+        except FileNotFoundError:
+            print(f'File: "{path}" cannot be found, txt will not be processed')
+            return []
+        except IndexError:
+            print(f'File: "{path}" cannot be parsed - wrong file structure')
+            return []
