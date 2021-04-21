889426887b261d073be66f54a739eeca36abab11
Bartosz Ostrowski <buniumasta@gmail.com>
2021-04-21 17:56:46 +0200

    BugCleaned

--- memegenerator/meme_engine.py
+++ memegenerator/meme_engine.py
@@ -43,19 +43,18 @@ class MemeEngine():
                     stroke_width=0,
                     embedded_color=False)
                 diff_x = int(img.size[0]-textsize[2])
-                print(quote)
-                print(img)
-                print('diff_x = img.size[0] - textsize[2]')
-                print(f'{diff_x} = {img.size[0]} - {textsize[2]}')
+
                 if diff_x < 0:
                     x_dim=0
                 else:
                     x_dim = randint(0, diff_x)
                 diff_y = int(img.size[1]-textsize[3])
+
                 if  diff_y < 0:
                     y_dim=0
                 else:
                     y_dim = randint(0, diff_y)
+
                 draw.multiline_text(
                     (x_dim, y_dim),
                     quote,
