310eb741c5fb253765c8c48f81e86bf9d80db986
Bartosz Ostrowski <buniumasta@gmail.com>
2021-04-21 12:08:10 +0200

    fuction_ready

--- quoteengine/ingestor_pdf.py
+++ quoteengine/ingestor_pdf.py
@@ -1,5 +1,7 @@
 """Implementation of PDF stragety for ingestor class."""
 from typing import List
+from subprocess import run
+from os.path import splitext
 from .ingestor_interface import IngestorInterface
 from .quote_model import QuoteModel
 
@@ -7,19 +9,32 @@ from .quote_model import QuoteModel
 class PDFIngestor(IngestorInterface):
     """Implements TXT strategy."""
 
-    _supported_files = ['txt']
+    _supported_files = ['pdf']
 
     @classmethod
     def parse(cls, path: str) -> List[QuoteModel]:
         """Parse the PDF file and return List of QuoteMode."""
         if not cls.can_ingest(path):
             return []
+        try:
+            run(["pdftotext", "-layout", "-eol", "unix", path], check=True)
+            newpath_tuple = splitext(path)
+            new_path = newpath_tuple[0] + '.txt'
+            quotemode_list = []
 
-        # subprocess.run
-        quotemode_list = []
-        with open(path, encoding='utf-8-sig') as file:
-            for line in file.readlines():
-                row = line.strip().split('-')
-                quote_author = QuoteModel(row[0].strip(), row[1].strip())
-                quotemode_list.append(quote_author)
-        return quotemode_list
+            with open(new_path) as file:
+                for line in file.readlines():
+                    if len(line) > 1:  # this line  should be re-factored consider regexx for 
+                        #print(line.strip())
+                        row = line.strip().split('-')
+                        #print(row)
+                        quote_author = QuoteModel(row[0].strip(), row[1].strip())
+                        quotemode_list.append(quote_author)
+            return quotemode_list
+
+        except FileNotFoundError as exc:
+            print(f'PDF cannot be found {exc}')
+            return []
+        except IndexError as exc:
+            print(f'PDF cannot be read - wrong file structure {exc}')
+            return []
