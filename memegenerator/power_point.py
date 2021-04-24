dff038c2f984504d22643166f976a77365d81264
Bartosz Ostrowski <buniumasta@gmail.com>
2021-04-24 16:32:26 +0200

    Added text

--- memegenerator/power_point.py
+++ memegenerator/power_point.py
@@ -35,8 +35,7 @@ def gen_ppt(img_path, mytxt):
     txBox = slide.shapes.add_textbox(left, top, width, height)
     tf = txBox.text_frame
 
-    tf.text = """The Picture on previous slide is captioned with sentence. 
-    \n The same text is used on this slide as well. See below \n' + mytxt"""
+    tf.text = "The Picture on previous slide is captioned with sentence. \n The same text is used on this slide as well. See below \n" + mytxt
 
     p = tf.add_paragraph()
     p.text = '"' + mytxt + '" \nThis text can be parsed in any way, for example can be reversed: \n' + mytxt[::-1] 
